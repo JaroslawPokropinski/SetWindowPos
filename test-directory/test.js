4ccedbeb7db1c385e427a74a784f81f5cb3b2407
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-08-13 21:19:40 +0200

    change package.json and rename tests

---
+++ test-directory/test.js
@@ -0,0 +1,3 @@
+var { SetWindowPos } = require('win-setwindowpos');
+
+SetWindowPos(0n, 1n, 0, 0, 0, 0, 0x0013);
--- test/testmodule.js
+++
@@ -1,3 +0,0 @@
-var { SetWindowPos } = require('../index.js');
-
-SetWindowPos(0n, 1n, 0, 0, 0, 0, 0x0013);
