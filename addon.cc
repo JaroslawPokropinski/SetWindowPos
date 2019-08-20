9be2262f9a819f8db62edf4bd6eda31398fb72cb
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-08-20 23:44:24 +0200

    1.3.0

--- addon.cc
+++ addon.cc
@@ -1,53 +1,78 @@
 #include <windows.h>
-#include <node.h>
+#define NAPI_EXPERIMENTAL
+#include <node_api.h>
+#include <string>
 
 namespace setWindowPos
 {
-
-    using v8::Exception;
-    using v8::FunctionCallbackInfo;
-    using v8::Isolate;
-    using v8::Local;
-    using v8::NewStringType;
-    using v8::Number;
-    using v8::Object;
-    using v8::String;
-    using v8::Value;
-    using v8::BigInt;
-
-    void Method(const FunctionCallbackInfo<Value> &args)
+    napi_value Method(napi_env env, napi_callback_info args)
     {
-        Isolate *isolate = args.GetIsolate();
-        if (args.Length() < 7)
+        napi_status status;
+
+        size_t argc = 7;
+        napi_value argv[7];
+        status = napi_get_cb_info(env, args, &argc, argv, NULL, NULL);
+        if (status != napi_ok)
         {
-            // Throw an Error that is passed back to JavaScript
-            isolate->ThrowException(Exception::TypeError(
-                String::NewFromUtf8(isolate,
-                                    "Wrong number of arguments",
-                                    NewStringType::kNormal)
-                    .ToLocalChecked()));
-            return;
-        }
-        if (!args[0]->IsBigInt() || !args[1]->IsBigInt() || !args[2]->IsNumber() || !args[3]->IsNumber() || !args[4]->IsNumber() || !args[5]->IsNumber() || !args[6]->IsNumber())
-        {
-            isolate->ThrowException(Exception::TypeError(
-                String::NewFromUtf8(isolate,
-                                    "Wrong arguments",
-                                    NewStringType::kNormal)
-                    .ToLocalChecked()));
-            return;
+            napi_throw_error(env, NULL, "Failed to parse arguments");
         }
 
-        SetWindowPos((HWND)args[0].As<BigInt>()->Uint64Value(), (HWND)args[1].As<BigInt>()->Uint64Value(), (int)args[2].As<Number>()->Value(),
-                     (int)args[3].As<Number>()->Value(), (int)args[4].As<Number>()->Value(), (int)args[5].As<Number>()->Value(),
-                     (UINT)args[6].As<Number>()->Value());
+        bool loosless;
+
+        uint64_t arg0;
+        status = napi_get_value_bigint_uint64(env, argv[0], &arg0, &loosless);
+        if (status != napi_ok || !loosless)
+        {
+            napi_throw_error(env, NULL, "argv[0] should be 64bit bigint");
+        }
+        HWND hwnd = (HWND) arg0;
+
+        uint64_t arg1;
+        status = napi_get_value_bigint_uint64(env, argv[1], &arg1, &loosless);
+        if (status != napi_ok || !loosless)
+        {
+            napi_throw_error(env, NULL, "argv[1] should be 64bit bigint");
+        }
+        HWND after = (HWND)arg1;
+
+        int32_t intArgs[4];
+        for (int i = 0; i < 4; i++)
+        {
+            status = napi_get_value_int32(env, argv[2 + i], &intArgs[i]);
+            if (status != napi_ok)
+            {
+                napi_throw_error(env, NULL, ("argv[" + std::to_string(2 + i) + "] should be integer").c_str());
+            }
+        }
+
+        uint32_t flag;
+        status = napi_get_value_uint32(env, argv[6], &flag);
+        if (status != napi_ok)
+        {
+            napi_throw_error(env, NULL, "argv[6] should be unsigned integer");
+        }
+
+        SetWindowPos(hwnd, after, intArgs[0], intArgs[1], intArgs[2], intArgs[3], flag);
+        napi_value nullVal;
+        status = napi_create_int32(env, 0, &nullVal);
+        return nullVal;
     }
 
-    void Initialize(Local<Object> exports)
+    napi_value init(napi_env env, napi_value exports)
     {
-        NODE_SET_METHOD(exports, "SetWindowPos", Method);
+        napi_status status;
+        napi_value fn;
+
+        status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
+        if (status != napi_ok)
+            return nullptr;
+
+        status = napi_set_named_property(env, exports, "SetWindowPos", fn);
+        if (status != napi_ok)
+            return nullptr;
+        return exports;
     }
 
-    NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+    NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
 
 } // namespace setWindowPos
